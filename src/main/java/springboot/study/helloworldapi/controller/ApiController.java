f93b0fc62ed713961edbc5753356354aeeb8bf03
홍준성 <ghdwnstjd13@gmail.com>
2022-08-28 18:11:35 +0900

    Hello World API

---
+++ src/main/java/springboot/study/helloworldapi/controller/ApiController.java
@@ -0,0 +1,15 @@
+package springboot.study.helloworldapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // 해당 Class는 REST API를 처리하는 Controller로 인식시킨다.
+@RequestMapping("/api") // RequestMapping URI를 지정해주는 Annotation
+public class ApiController {
+
+    @GetMapping("/hello") // http://localhost:{port}/api/hello
+    public String hello() {
+        return "hello spring boot!";
+    }
+}
